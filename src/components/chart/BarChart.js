7b010c5c9cfeb9df0199594ae5fe14194cea914e
Valerian <valgerin@hotmail.fr>
2021-03-13 19:28:23 +0100

    props treatment and error message

--- src/components/chart/BarChart.js
+++ src/components/chart/BarChart.js
@@ -1,7 +1,31 @@
 import React from "react";
+import Chart from "react-google-charts";
+import { caption, options } from "../../utils/ChartOptions";
+import style from "./BarChart.module.scss";
 
-const BarChart = () => {
-  return <div></div>;
+const BarChart = ({ data }) => {
+  /*Updating the NEOs data catching from props to match with the expected pattern in the data option on Chart */
+  const neosData = data?.map((neo) => {
+    return [neo.name, neo.size.min, neo.size.max];
+  });
+
+  const neosDataWithCaption = [caption].concat(neosData);
+
+  return (
+    <div className={style.container}>
+      {data && data.length > 1 ? (
+        <Chart
+          width={"1200px"}
+          height={"920px"}
+          chartType="BarChart"
+          data={neosDataWithCaption}
+          options={options}
+        />
+      ) : (
+        <div>Donnée non disponibles</div>
+      )}
+    </div>
+  );
 };
 
 export default BarChart;
