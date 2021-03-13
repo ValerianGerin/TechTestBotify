046e20850fb6e1d1361b7e33e31e6d97213bb33e
Valerian <valgerin@hotmail.fr>
2021-03-13 18:52:13 +0100

    calling endpoint and filtering the data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
 import React, { useState, useEffect } from "react";
-
+import { BarChart, Loader } from "./components";
+import { average } from "./utils/functionLib";
 const App = () => {
-
   const [data, setData] = useState();
   const [isLoaded, setIsLoaded] = useState(false);
+  const [message, setMessage] = useState();
+  
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch(
-        "https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=DEMO_KEY"
-      );
-      const jsonResponse = await response.json();
-      setData(jsonResponse);
-      setIsLoaded(true)
+      try {
+        const response = await fetch(
+          "https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=DEMO_KEY"
+        );
+        const jsonResponse = await response.json();
+  
+        setIsLoaded(true);
+  
+        const filteredData = jsonResponse.near_earth_objects
+          .map((neo) => {
+            let name = neo.name;
+            let minSize =
+              neo.estimated_diameter.kilometers.estimated_diameter_min;
+            let maxSize =
+              neo.estimated_diameter.kilometers.estimated_diameter_max;
+            let averageSize = average(minSize, maxSize);
+            return {
+              name: name,
+              size: { min: minSize, max: maxSize, average: averageSize },
+            };
+          })
+          .sort((curr, next) => next.size.average - curr.size.average);
+        setData(filteredData);
+      } catch (error) {
+        if(error){
+          setMessage("Désolé votre requete n'a pu aboutir")
+        }
+      }
     };
+
     fetchData();
   }, []);
   console.log(data);
 
-  return <></>;
+  return <>{isLoaded ? <BarChart /> : <Loader />}{message}</>;
 };
 
 export default App;
