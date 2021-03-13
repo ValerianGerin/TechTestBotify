b67f4463292a38daff7c7252252339b4ae346e85
Valerian <valgerin@hotmail.fr>
2021-03-13 19:29:07 +0100

    setting up option for Chart

---
+++ src/utils/ChartOptions.js
@@ -0,0 +1,19 @@
+export const caption = [
+  "NEOs",
+  "Min Estimated Diameter (km)",
+  "Max Estimated Diameter (km)",
+];
+
+
+
+export const options = {
+  chartArea: { width: "50%" },
+  hAxis: {
+    title: "Min Estimated Diameter (km)",
+    minValue: 0,
+    
+  },
+  vAxis: {
+    title: "NEO name",
+  },
+};
