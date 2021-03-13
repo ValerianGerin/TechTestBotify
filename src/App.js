efd6b90ac7197920040e7e24cdfe08bf84811de6
Valerian <valgerin@hotmail.fr>
2021-03-13 18:27:29 +0100

    setting up api call in useEffect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
+const App = () => {
 
-function App() {
-  return (
-    <div>
-     
-    </div>
-  );
-}
+  const [data, setData] = useState();
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        "https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=DEMO_KEY"
+      );
+      const jsonResponse = await response.json();
+      setData(jsonResponse);
+      setIsLoaded(true)
+    };
+    fetchData();
+  }, []);
+  console.log(data);
+
+  return <></>;
+};
 
 export default App;
