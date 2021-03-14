45df05f7d8943e51b86e7cf26d7e5352d41b2a7d
Valerian <valgerin@hotmail.fr>
2021-03-14 16:29:28 +0100

    setting up dropdown component

---
+++ src/components/dropdown/Dropdown.js
@@ -0,0 +1,27 @@
+import React, {useState} from "react";
+import style from "./Dropdown.module.scss";
+
+const Dropdown = () => {
+  const [orbitalBody, setOrbitalBody] = useState("Earth")
+
+  const handleChange = (e)=>{
+    const {id} = e.target;
+ 
+
+  }
+  return (
+    <div className={style.container}>
+      <div className={style.title}>Orbiting body: {orbitalBody}</div>
+      <input placeholder="Type to search" className={style.searchOrbitalBody} />
+      <hr />
+      <form>
+        <div id="Earth" onClick={handleChange}>Earth</div>
+        <div id="Juptr" onClick={handleChange}>Juptr</div>
+        <div id="Mars" onClick={handleChange}>Mars</div>
+        <div id="Merc" onClick={handleChange}>Merc</div>
+      </form>
+    </div>
+  );
+};
+
+export default Dropdown;
