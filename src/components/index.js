ba2e53e6df875ae3f8b6d35a5e4fb832537c6865
Valerian <valgerin@hotmail.fr>
2021-03-14 16:43:39 +0100

    export dropdown from index

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
-export {default as BarChart} from "./chart/BarChart";
-export {default as Loader} from "./loader/Loader";
+export { default as BarChart } from "./chart/BarChart";
+export { default as Loader } from "./loader/Loader";
+export { default as Dropdown } from "./dropdown/Dropdown";
